2d0d1674e7d3c12b388d014a80dd342ba21fd66b
Roman Senin <roman@launchbadge.com>
2021-06-23 12:14:47 -0700

    add course card widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:new_app/constants.dart';
+
+import 'model/course.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,8 +13,111 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            child: RecentCourseCard(
+              course: recentCourses[2],
+            ),
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class RecentCourseCard extends StatelessWidget {
+  RecentCourseCard({this.course});
+
+  Course course;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.topRight,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(top: 20.0),
+          child: Container(
+            width: 240,
+            height: 240,
+            decoration: BoxDecoration(
+              gradient: course.background,
+              borderRadius: BorderRadius.circular(41.0),
+              boxShadow: [
+                BoxShadow(
+                  color: course.background.colors[0].withOpacity(
+                    0.3,
+                  ),
+                  offset: Offset(0, 20),
+                  blurRadius: 30,
+                ),
+                BoxShadow(
+                  color: course.background.colors[1].withOpacity(
+                    0.3,
+                  ),
+                  offset: Offset(0, 20),
+                  blurRadius: 30,
+                ),
+              ],
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 32.0,
+                    left: 32.0,
+                    right: 32.0,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        course.courseSubtitle,
+                        style: kCardSubtitleStyle,
+                      ),
+                      SizedBox(
+                        height: 6.0,
+                      ),
+                      Text(
+                        course.courseTitle,
+                        style: kCardTitleStyle,
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Image.asset(
+                    'asset/illustrations/${course.illustration}',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(
+            right: 42,
+          ),
+          child: Container(
+            child: Image.asset('asset/logos/${course.logo}'),
+            width: 60.0,
+            height: 60.0,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(18.0),
+              boxShadow: [
+                BoxShadow(
+                  color: kShadowColor,
+                  offset: Offset(0, 4),
+                  blurRadius: 16.0,
+                )
+              ],
+            ),
+            padding: EdgeInsets.all(12.0),
+          ),
+        ),
+      ],
     );
   }
 }
