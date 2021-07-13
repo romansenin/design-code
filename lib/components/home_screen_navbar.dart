01c66d451727fe8fe2eb801ec9a1b00aa3b9ce71
Roman Senin <roman@launchbadge.com>
2021-07-13 11:10:17 -0700

    refactor into sub-widgets, add listview for explore course cards

---
+++ lib/components/home_screen_navbar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:new_app/components/searchfield_widget.dart';
+import 'package:new_app/components/sidebar_button.dart';
+
+import '../constants.dart';
+
+class HomeScreenNavBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SidebarButton(),
+          SearchFieldWidget(),
+          Icon(
+            Icons.notifications,
+            color: kPrimaryLabelColor,
+          ),
+          SizedBox(width: 16),
+          CircleAvatar(
+            radius: 18,
+            backgroundImage: AssetImage('asset/images/profile.jpg'),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/components/sidebar_button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+class SidebarButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      onPressed: () {
+        print("Sidebar button pressed");
+      },
+      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      highlightColor: Colors.transparent,
+      splashColor: Colors.transparent,
+      constraints: BoxConstraints(
+        maxWidth: 40,
+        maxHeight: 40,
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(14),
+          boxShadow: [
+            BoxShadow(
+              color: kShadowColor,
+              offset: Offset(0, 12),
+              blurRadius: 16,
+            )
+          ],
+        ),
+        child: Image.asset(
+          'asset/icons/icon-sidebar.png',
+          color: kPrimaryLabelColor,
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 14),
+      ),
+    );
+  }
+}
