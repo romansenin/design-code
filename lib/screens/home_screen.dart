9472a15fc2c68ce32678607b750b9922839d319e
Roman Senin <roman@launchbadge.com>
2021-07-16 10:40:55 -0700

    use slide_up_panel package

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:new_app/components/lists/recent_course_list.dart';
 import 'package:new_app/screens/sidebar_screen.dart';
 
 import '../constants.dart';
+import 'continue_watching_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -118,6 +119,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                 ],
               ),
             ),
+            ContinueWatchingScreen(),
             IgnorePointer(
               ignoring: sidebarHidden,
               child: Stack(
